c4e81f660c0909a9e801add7e1441fd21460889a
Silvia Rus <47898287+Silvia-Rus@users.noreply.github.com>
2024-04-21 19:24:39 -0300

    Fracciones (sobrecarga de operadodores)

---
+++ Fracciones/Fracciones/Fraccion.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Fracciones
+{
+	public class Fraccion
+	{
+		public int num;
+		public int den;
+
+		public Fraccion(int num, int den)
+        {
+
+            if (den == 0)
+            {
+                throw new ArgumentException("BOOOM EL DENOMINADOR NO PUEDE SER CERO!!!");
+            }
+
+            this.num = num;
+            this.den = den;
+        }
+        public static Fraccion operator +(Fraccion a, Fraccion b)
+        {
+            int num = (a.num * b.den) + (a.den * b.num);
+            int den = a.den * b.den;
+            Fraccion retorno = new Fraccion(num, den);
+            return retorno;
+        }
+
+        public static Fraccion operator *(Fraccion a, Fraccion b)
+        {
+            int num = a.num * b.num;
+            int den = a.den * b.den;
+            Fraccion retorno = new Fraccion(num, den);
+            return retorno;
+        }
+
+    }
+}
+
---
+++ Fracciones/Fracciones/Program.cs
@@ -0,0 +1,31 @@
+﻿using Fracciones;
+namespace Fracciones;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.Title = "Fracciones";
+
+        Console.WriteLine("SUMA");
+        Fraccion fraccionUno = new Fraccion(1, 5);
+        Fraccion fraccionDos = new Fraccion(2, 3);
+        Fraccion resultado = fraccionUno + fraccionDos;
+        Console.WriteLine(resultado.num + "/" + resultado.den);
+
+        //Console.WriteLine("MULTIPLICACIÓN");
+        //Fraccion fraccionUno = new Fraccion(7, 8);
+        //Fraccion fraccionDos = new Fraccion(9, 5);
+        //Fraccion resultado = fraccionUno * fraccionDos;
+        //Console.WriteLine(resultado.num+"/"+resultado.den);
+
+
+        //Console.WriteLine("CONSTRUIR UNA FRACCIÓN QUE EXPLOTA");
+        //Fraccion fraccionBOOM = new Fraccion(7, 0);
+
+
+
+    }
+}
+
