9185e6ddddc3533bdfca000137255de7777b8cc9
Silvia Rus <47898287+Silvia-Rus@users.noreply.github.com>
2024-04-21 20:26:51 -0300

    bolígrafo

---
+++ EJ17_Boligrafo/EJ17_Boligrafo/Boligrafo.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boligrafo
+{
+    public class Boligrafo
+    {
+        const short cantidadTintaMaxima = 100;
+        ConsoleColor color;
+        short tinta;
+
+        public Boligrafo(short tinta, ConsoleColor color)
+        {
+            this.tinta = tinta;
+            this.color = color;
+        }
+
+        public ConsoleColor getColor()
+        {
+            return this.color;
+        }
+
+        public short getTinta()
+        {
+            return this.tinta;
+        }
+
+        public short Tinta
+        {
+            get => this.tinta;
+            set
+            {
+
+            }
+        }
+
+        public bool Pintar(short gasto, out string dibujo)
+        {
+            dibujo = "";
+            bool retorno = false;
+            for (int i = 0; i < gasto; i++)
+            {
+                if (this.getTinta() > 0)
+                {
+                    dibujo += "*";
+                    this.setTinta(-1);
+                    Console.ForegroundColor = this.getColor();
+                    retorno = true;
+                }
+                else
+                {
+                    if (i > 0)
+                    {
+                        break;
+                    }
+                }
+            }
+            return retorno;
+        }
+
+        public void Recargar()
+        {
+            this.setTinta(cantidadTintaMaxima);
+        }
+
+        //SET TINTA POR ESCENARIOS
+        private void setTinta(short tinta)
+        {
+            int validador = this.tinta + tinta;
+
+            if (validador < cantidadTintaMaxima && validador > -1) //E1: La operación no sale de rango ni por arriba ni por abajo
+            {
+                this.tinta += tinta;
+            }
+            else if (validador >= cantidadTintaMaxima) //E2: La operación hacía que pusiéramos más del máximo de tina
+            {
+                this.tinta = cantidadTintaMaxima;
+            }
+            else //E3: La operación gasta toda la tinra
+            {
+                this.tinta = 0;
+            }
+        }
+
+
+    }
+}
---
+++ EJ17_Boligrafo/EJ17_Boligrafo/Program.cs
@@ -0,0 +1,22 @@
+﻿using Boligrafo;
+using System.Globalization;
+
+namespace Ej17_Boligrafo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Boligrafo.Boligrafo boliUno = new Boligrafo.Boligrafo(10, ConsoleColor.Blue);
+            Boligrafo.Boligrafo boliDos = new Boligrafo.Boligrafo(50, ConsoleColor.Red);
+
+            String dibujoUno;
+            boliUno.Pintar(9, out dibujoUno);
+            Console.WriteLine(dibujoUno); //MUESTRA 2
+            boliUno.Recargar();
+            Console.WriteLine(boliUno.getTinta()); //MUESTRA 100
+
+            Console.ReadKey();
+        }
+    }
+}
