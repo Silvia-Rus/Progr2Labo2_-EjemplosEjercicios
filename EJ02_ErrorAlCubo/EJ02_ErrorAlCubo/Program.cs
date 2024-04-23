fc77241257df47358cda93653f250e6897fb4d90
Silvia Rus <47898287+Silvia-Rus@users.noreply.github.com>
2024-04-23 14:06:26 -0300

    Ejercicio 2"

---
+++ EJ02_ErrorAlCubo/EJ02_ErrorAlCubo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.ConstrainedExecution;
+
+namespace EJ02_ErrorAlCubo;
+
+class Program
+{
+    //Ingresar un número y mostrar el cuadrado y el cubo del mismo.
+    //Se debe validar que el número sea mayor que cero,
+    //caso contrario, mostrar el mensaje: "ERROR. ¡Reingresar número!".
+
+    static void Main(string[] args)
+    {
+        int numero;
+        Console.WriteLine("Ingrese un número");
+        string entrada = Console.ReadLine();
+        while(!int.TryParse(entrada, out numero) || numero < 1)
+        {
+            Console.WriteLine("ERROR. ¡Reingresar número!");
+            entrada = Console.ReadLine();
+        }
+
+        double cuadrado = Math.Pow(numero, 2);
+        double cubo = Math.Pow(numero, 3);
+
+        Console.WriteLine("El cuadrado del numero introducido es: {0} ", cuadrado);
+        Console.WriteLine("El cubo del numero introducido es: " + cubo);
+
+
+        Console.ReadKey();
+    }
+}
+
